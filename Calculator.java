926ac47ebed36352d37c03330924882d4975b374
Darpan <darpan.madlani@gmail.com>
2022-06-30 20:55:28 +0530

    Addition module is developed

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,15 @@
 public class Calculator {
+ 
+	public double add(double num1, double num2) {
+		return num1+num2;
+	}
 
 	public static void main(String[] args) {
-	
+
+		Calculator calculator = new Calculator();
+		double result = calculator.add(12.5,14.7);
+		System.out.println(result);
 		
-	
 	}
 
 }
