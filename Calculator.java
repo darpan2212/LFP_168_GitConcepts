b818c2d9d4737c89b02e6ae8e22c024499c1b0b8
Darpan <darpan.madlani@gmail.com>
2022-06-30 21:05:25 +0530

    multiplication module is done

--- Calculator.java
+++ Calculator.java
@@ -4,11 +4,18 @@ public class Calculator {
 		return num1+num2;
 	}
 
+	public double multiply(double num1, double num2) {
+		return num1*num2;
+	}
+
 	public static void main(String[] args) {
 
 		Calculator calculator = new Calculator();
 		double result = calculator.add(12.5,14.7);
-		System.out.println(result);
+		System.out.println("Addition is : "+result);
+		
+		result = calculator.multiply(12.5,14.7);
+		System.out.println("Multiplication is : "+result);
 		
 	}
 
